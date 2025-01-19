a36b56ed4cdc3eff248bfe98fedbca98639b17aa
Melissa Perez <perezmelissa053@gmail.com>
2025-01-18 21:31:50 -0500

    Working on update action

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,11 +3,19 @@ const Job = require("../models/Job");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-    res.send("get all jobs");
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+    return res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
-    res.send("get job");
+    const { user: { userId }, params: { id: jobId } } = req;
+    const job = await Job.findOne({
+        _id: jobId, createdBy: userId
+    });
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`);
+    }
+    return res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
@@ -17,7 +25,17 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-    res.send("update job");
+    const {
+        body: { company, position },
+        user: { userId },
+        params: { id: jobId }
+    } = req;
+
+    if (company === "" || position === "") {
+        throw new BadRequestError("Company or position fields cannot be empty");
+    }
+
+    const job = await Job.findByIdAndUpdate({});
 };
 
 const deleteJob = async (req, res) => {
