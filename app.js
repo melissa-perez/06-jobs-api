58057bad1d6923d25e137cc8409b791770df7ce0
Melissa Perez <perezmelissa053@gmail.com>
2025-01-20 01:34:15 -0500

    Finish up to render deployment, double check app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
-const rateLimit = require("express-rate-limit");
+const rateLimiter = require("express-rate-limit");
 const app = express();
 
 const connectDB = require("./db/connect");
@@ -15,12 +15,16 @@ const authenticateUser = require("./middleware/authentication");
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set("trust proxy", 1);
+app.use(rateLimiter({
+  windowsMs: 15 * 60 * 1000,
+  max: 100
+}));
 app.use(express.json());
-// extra packages
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(rateLimit);
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
