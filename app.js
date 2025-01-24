f5ef35762409bc610556a9669942d88891bbf3d0
Melissa Perez <perezmelissa053@gmail.com>
2025-01-24 01:49:50 -0500

    Add swagger docs

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerYaml = YAML.load("./swagger.yaml");
 const app = express();
 
 const connectDB = require("./db/connect");
@@ -15,14 +18,18 @@ const authenticateUser = require("./middleware/authentication");
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.set("trust proxy", 1);
+const isProductionEnv = app.get("env") === "production";
+if (isProductionEnv) {
+  app.set("trust proxy", 1);
+  app.use(cors());
+}
+
 app.use(rateLimiter({
   windowsMs: 15 * 60 * 1000,
   max: 100
 }));
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 app.use(xss());
 
 // routes
@@ -31,6 +38,7 @@ app.get('/', (req, res) => {
 });
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerYaml));
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
